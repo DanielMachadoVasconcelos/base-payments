0984356955903f0521c9e4ba42494020a4d21ef6
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2024-11-10 14:48:24 +0100

    BP-004 Attempt to create a cloud gateway application

--- src/main/java/com/ead/payments/Application.java
+++ src/main/java/com/ead/payments/Application.java
@@ -3,9 +3,12 @@ package com.ead.payments;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.scheduling.annotation.EnableAsync;
 
+
 @EnableAsync
+@EnableDiscoveryClient
 @SpringBootApplication
 @EnableConfigurationProperties
 public class Application {
--- src/main/java/com/ead/payments/products/ProductCreatedEvent.java
+++ src/main/java/com/ead/payments/products/ProductCreatedEvent.java
@@ -1,15 +1,20 @@
 package com.ead.payments.products;
 
 import com.ead.payments.eventsourcing.BaseEvent;
+import com.fasterxml.jackson.annotation.JsonCreator;
 import java.util.List;
 import java.util.UUID;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.ToString;
+import org.springframework.modulith.events.Externalized;
 
 @Data
-@AllArgsConstructor
+@ToString
 @NoArgsConstructor
+@Externalized("products-events.v1.topic::#{getId().toString()}")
+@AllArgsConstructor(onConstructor_ = @JsonCreator)
 public class ProductCreatedEvent extends BaseEvent {
 
     private UUID id;
