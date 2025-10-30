bde2acf0265b9d5da51ce71e21af7663c0961f67
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2025-10-30 21:21:03 +0100

    BP-0016 Fixed the failing test that was using currency as a string instead of the object
    Signed-off-by: Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>

--- src/test/java/com/ead/payments/architecture/ClassesNamesConventionTest.java
+++ src/test/java/com/ead/payments/architecture/ClassesNamesConventionTest.java
@@ -1,11 +1,11 @@
 package com.ead.payments.architecture;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
-
 import com.tngtech.archunit.core.importer.ImportOption;
 import com.tngtech.archunit.junit.AnalyzeClasses;
 import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
+
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.classes;
 
 @AnalyzeClasses(packages = "com.ead.payments", importOptions = ImportOption.DoNotIncludeTests.class)
 class ClassesNamesConventionTest {
@@ -27,6 +27,7 @@ class ClassesNamesConventionTest {
                     .orShould().haveSimpleNameEndingWith("Product")
                     .orShould().haveSimpleNameEndingWith("Controller")
                     .orShould().haveSimpleNameEndingWith("Listener")
+                    .orShould().haveSimpleNameEndingWith("Decorator")
                     .orShould().haveSimpleNameEndingWith("Service")
                     .orShould().haveSimpleNameEndingWith("Repository")
                     .orShould().haveSimpleNameEndingWith("Configuration")
