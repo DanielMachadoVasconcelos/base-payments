90577875d5c3b4ea4f46d393e2865ad0906ec7db
Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>
2025-11-11 21:08:17 +0100

    BP-0016 Removed the parallel tests
    Signed-off-by: Daniel Machado Vasconcelos <danielm.vasconcelos@gmail.com>

--- src/test/java/com/ead/payments/SpringBootIntegrationTest.java
+++ src/test/java/com/ead/payments/SpringBootIntegrationTest.java
@@ -2,8 +2,6 @@ package com.ead.payments;
 
 import com.ead.payments.mocks.WireMockProvider;
 import com.github.tomakehurst.wiremock.WireMockServer;
-import org.junit.jupiter.api.parallel.Execution;
-import org.junit.jupiter.api.parallel.ExecutionMode;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
@@ -16,7 +14,6 @@ import static com.ead.payments.mocks.WireMockProvider.IssuerServiceMockProvider;
 @SpringBootTest
 @AutoConfigureMockMvc
 @ActiveProfiles("integration-test")
-@Execution(ExecutionMode.CONCURRENT)
 @EnableWireMock({@ConfigureWireMock(name = "issuer-service", baseUrlProperties = "issuer.client.base-url")})
 public class SpringBootIntegrationTest {
 
